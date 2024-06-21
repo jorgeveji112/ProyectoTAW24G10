4811a065ab57521f0888df3fb1e9910919671129
Jorgeveji11 <jorgeveji@uma.es>
2024-06-21 17:45:53 +0200

    RutinaClienteController con service

--- ProyectoTaw/src/main/java/es/uma/proyectotaw/service/SesionEjercicioService.java
+++ ProyectoTaw/src/main/java/es/uma/proyectotaw/service/SesionEjercicioService.java
@@ -1,7 +1,22 @@
 package es.uma.proyectotaw.service;
 
+import es.uma.proyectotaw.dao.SesionejercicioRepository;
+import es.uma.proyectotaw.dto.SesionejercicioDTO;
+import es.uma.proyectotaw.entity.SesionejercicioEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SesionEjercicioService {
+
+    @Autowired
+    SesionejercicioRepository sesionejercicioRepository;
+
+    public SesionejercicioDTO buscarPorId(Integer ejercicioId) {
+        SesionejercicioEntity sesionejercicio = sesionejercicioRepository.findById(ejercicioId).orElse(null);
+        if (sesionejercicio == null){
+            return null;
+        }
+        return sesionejercicio.toDTO();
+    }
 }
